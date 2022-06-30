c596f3c34a0702a7278d6ed1bdca01305e2a2071
Carlos Alberto Dantas Filho <carlosdantas485@gmail.com>
2022-06-30 13:41:22 -0300

    layout V3.0 bank

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,50 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './componentes/template/header/header.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './componentes/template/footer/footer.component';
+import { NavComponent } from './componentes/template/nav/nav.component';
+import { MatSidenavModule } from'@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MatCardModule } from'@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatSlideToggleModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatBadgeModule,
+    MatStepperModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
